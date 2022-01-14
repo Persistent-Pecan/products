f493eacf99505c64af754368b3893dcda7892ad9
yulanrong <yulanrong123@gmail.com>
2022-01-13 23:44:56 -0800

    etl process

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,16 @@
 -- psql postgres
--- psql -d productdb -f schema.sql
-drop database if exists productdb;
-create database productdb;
+-- psql -d productdb  -a -f schema.sql
+--drop database if exists productdb;
+--create database productdb;
 
+--psql -d productdb -U username -W
 \connect productdb;
+
+drop table if exists products cascade;
+drop table if exists features cascade;
+drop table if exists related cascade;
+drop table if exists styles cascade;
+drop table if exists photos cascade;
 
 create table products (
   product_id int not null primary key,
@@ -11,7 +18,7 @@ create table products (
   slogan varchar(250),
   description varchar(500),
   category varchar(100) not null,
-  default_price real check(default_price > 0)
+  default_price int
 );
 
 create table features (
@@ -26,24 +33,48 @@ create table related (
   id int not null primary key,
   product_id int not null,
   related_id int,
-  foreign key(product_id) references products(product_id),
-  foreign key(related_id) references products(product_id),
+  foreign key(product_id) references products(product_id)
 );
 
 create table styles (
   style_id int not null primary key,
   product_id int not null,
   name varchar(50) not null,
-  original_price real check(original_price > 0),
-  sale_price real check(sale_price < original_price),
-  default boolean not null,
+  sale_price varchar(10),
+  original_price int,
+  default_style boolean,
   foreign key(product_id) references products(product_id)
 );
 
 create table photos (
   id int not null primary key,
-  style_id int not null,
-  thumbnail_url varchar,
+  style_id int,
   url varchar,
+  thumbnail_url varchar,
   foreign key(style_id) references styles(style_id)
 );
+
+truncate products cascade;
+\copy products from '../data/product.csv' delimiter ',' csv header;
+
+truncate features cascade;
+\copy features from '../data/features.csv' delimiter ',' csv header;
+
+truncate related cascade;
+\copy related from '../data/related.csv' delimiter ',' csv header;
+
+truncate styles cascade;
+\copy styles from '../data/styles.csv' delimiter ',' csv header;
+
+truncate photos cascade;
+\copy photos from '../data/photos.csv' delimiter ',' csv header;
+
+update styles set sale_price = 0 where sale_price = 'null';
+alter table styles alter column sale_price type integer USING sale_price::integer;
+delete from styles where original_price < sale_price;
+
+--COPY 1000011
+--COPY 2219279
+--COPY 4508263
+--COPY 1958102
+--COPY 5655463
