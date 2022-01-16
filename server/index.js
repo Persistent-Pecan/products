4b08b5e9eed456f1cbf1aabf7e93decb88a600e8
yulanrong <yulanrong123@gmail.com>
2022-01-16 14:58:51 -0800

    4 routes setup

--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/products', model.getAllProducts);
-app.get('/products/get');
-app.get('/products/styles');
-app.get('/products/related');
+app.get('/products/:product_id', model.getProduct);
+app.get('/products/:product_id/styles', model.getProductStyles);
+app.get('/products/:product_id/related', model.getRelatedProducts);
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
--- server/model.js
+++ server/model.js
@@ -23,16 +23,73 @@ module.exports = {
     );
   },
 
-  getProduct: (req) => {
-
+  getProduct: (req, res) => {
+    const query = `select json_build_object(
+      'id', t.product_id,
+      'name', t.name,
+      'slogan', t.slogan,
+      'description', t.description,
+      'category', t.category,
+      'default_price', t.default_price,
+      'features', (select json_agg(row_to_json(features)) from (
+        select feature, value from features where product_id = t.product_id
+      )features)
+    )from (
+      select p.* from products p where p.product_id = ${req.params.product_id} and default_price > 0
+    ) t`;
+    db.client.query(query).then(
+      (results) => {
+        res.status(200).send(results.rows[0].json_build_object);
+      },
+    ).catch(
+      (err) => res.send(err.stack),
+    );
   },
-  getProductStyles: (req) => {
 
+  getProductStyles: (req, res) => {
+    const query = `select row_to_json(t) from (
+      select
+      product_id,
+      json_agg(json_build_object(
+        'style_id', s.style_id,
+        'name', s.name,
+        'original_price', s.original_price,
+        'sale_price', s.sale_price,
+        'default?', s.default_style,
+        'photos', (
+          select coalesce(photos, '[]')
+          from (
+            select json_agg(json_build_object(
+              'thumbnail_url', photos.thumbnail_url,
+              'url', photos.url
+            )) as photos from photos where photos.style_id = s.style_id
+          ) as photos
+        )
+      )) as results
+      from styles s
+      where product_id = ${req.params.product_id} and original_price > 0 group by product_id) t`;
+
+    db.client.query(query).then(
+      (results) => {
+        res.status(200).send(results.rows[0].row_to_json);
+      },
+    ).catch(
+      (err) => res.send(err.stack),
+    );
   },
-  getRelatedProducts: (req) => {
-
+  getRelatedProducts: (req, res) => {
+    const query = `
+    select json_agg(related.related_id)
+    from related
+    join products on products.product_id = related.product_id
+    where products.product_id = ${req.params.product_id}`;
+    db.client.query(query).then(
+      (results) => {
+        res.status(200).send(results.rows[0].json_agg);
+      },
+    ).catch(
+      (err) => res.send(err.stack),
+    );
   },
 
 };
-
-// db.client.query('', [params]).then(results => res.send(results)).catch(e => res.send(e));
